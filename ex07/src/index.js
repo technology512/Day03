4ccb105ff36428badf8b8d292f380d819344a756
technology512 <ald.wapper@gmail.com>
2021-03-10 23:06:33 +0100

    dodao sam folder ex07

---
+++ ex07/src/index.js
@@ -0,0 +1,31 @@
+function main () {
+    
+    //Declarations
+
+    var studlyCapVar; 
+    var properCamelCase; 
+    var titleCaseOver; 
+
+    //Assignments
+
+    var studlyCapVar = 10; 
+    var properCamelCase = "A String";
+    var titleCaseOver = 9000; 
+
+
+
+
+    
+     
+
+    
+return {
+    studlyCapVar,
+    properCamelCase,
+    titleCaseOver
+}
+    
+}
+console.log(main()); 
+
+module.exports = main;
