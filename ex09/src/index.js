e96018bf68fa825b2b5fd773fba1ef52071cd041
technology512 <ald.wapper@gmail.com>
2021-03-10 23:36:42 +0100

    dodao sam folder ex09

---
+++ ex09/src/index.js
@@ -0,0 +1,17 @@
+unction main (numOne, numTwo) {
+    
+    var numOne; 
+    var numTwo;
+    var difference; 
+
+    difference = numOne - numTwo ; 
+
+    return difference;
+
+    
+}
+console.log(main(numOne, numTwo)); 
+console.log(main(numOne, numTwo));
+console.log(main(numOne, numTwo));
+
+module.exports = main;
