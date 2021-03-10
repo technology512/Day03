d91901144958eea5fb7adc24dddd0e722075661c
technology512 <ald.wapper@gmail.com>
2021-03-10 21:27:24 +0100

    dodao folder ex02

---
+++ ex02/src/index.js
@@ -0,0 +1,14 @@
+function main () {
+    //This is a single line Javascript comment
+
+    /* document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    document.write("This is a multi-line comment!");
+    */
+
+}
+console.log(main()); 
+
+module.exports = main;
