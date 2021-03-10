333f16af600fba4f506c39b412183067f04d4194
technology512 <ald.wapper@gmail.com>
2021-03-10 23:28:56 +0100

    dodao sam folder ex08

---
+++ ex08/src/index.js
@@ -0,0 +1,17 @@
+function main (numOne, numTwo) {
+    
+    var numOne; 
+    var numTwo;
+    var sum; 
+
+    sum = numOne + numTwo; 
+
+    return sum;
+
+    
+}
+console.log(main(numOne, numTwo)); 
+console.log(main(numOne, numTwo));
+console.log(main(numOne, numTwo));
+
+module.exports = main;
